15961df6442692da24ad95493248a0b803a83018
Imran Khan <toimrank@gmail.com>
2025-04-14 10:56:19 -0500

    Application properties

--- src/main/java/com/learnandcode/com/configserver/ConfigServerApplication.java
+++ src/main/java/com/learnandcode/com/configserver/ConfigServerApplication.java
@@ -1,5 +1,6 @@
 package com.learnandcode.com.configserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -7,6 +8,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
+
+	@Value("${custom.order}")
+	private String order;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
